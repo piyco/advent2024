66751870b35b2d809754a2c9623323f269cda43e
piyco <apezap2@gmail.com>
2024-12-02 12:25:25 -0600

    DAY 1 PART 2

--- day-01/src/bin/part2.rs
+++ day-01/src/bin/part2.rs
@@ -1,3 +1,24 @@
+use std::collections::HashMap;
+use std::fs;
+
 fn main() {
-    println!("p2");
+    let mut vec = Vec::new();
+    let mut map: HashMap<i32, i32> = HashMap::new();
+
+    let input = fs::read_to_string("./data/input.txt").unwrap();
+
+    let mut flag = true;
+    for word in input.split_whitespace() {
+        let num_word: i32 = word.parse().unwrap();
+        if flag {vec.push(num_word)}
+        if !flag {*map.entry(num_word).or_insert(0) += 1}
+        flag = !flag;
+    }
+
+    let distance: i32 = vec
+        .iter()
+        .map(|k| k*map.get(k).unwrap_or(&0))
+        .sum();
+
+    println!("{}", distance);
 }
